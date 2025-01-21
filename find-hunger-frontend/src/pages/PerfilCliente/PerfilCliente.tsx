af8c3b755a32a5993ddfe5ee122854ce7dd31ecb
Milton Cardim <158493319+DevMiltonCardim@users.noreply.github.com>
2025-01-21 11:53:26 -0300

    Bug do perfil resolvido

--- find-hunger-frontend/src/pages/PerfilCliente/PerfilCliente.tsx
+++ find-hunger-frontend/src/pages/PerfilCliente/PerfilCliente.tsx
@@ -7,82 +7,84 @@ import { ArrowFatLinesRight } from '@phosphor-icons/react'
 export function PerfilCliente() {
     return (
         <>
-        <body>
-            <header>
-                    <HeaderPrincipal/>
-            </header>
-            <main className={styles.ContainerMain}>
-                <label className={styles.Picture}>
-                    <input type="file" accept='imagem/*' className={styles.PictureInput}/>
-                    <div className={styles.ContainerIconiTexto}>
-                        <i><Camera size={40} color="#ff4900" /></i>
-                  </div>
-                </label>
-                <div className={styles.ContainerDivButtonSair}>
-                    <button>Sair <ArrowFatLinesRight size={20} /></button>
-                </div>
-
-                <div className={styles.ContainerDivButton}>
-                    <button>Chat</button>
-                </div>
-                <form action="">
-                    <div className={styles.ContainerInput}>
-                        <div className={styles.ContainerTextoInput}>
-                            <p>Nome do produto</p>
-                            <input type="text" placeholder='adicione aqui'/>
-                        </div>
-                        <div className={styles.ContainerLinha}>
-                            <hr />
+            <body>
+                <header>
+                    <HeaderPrincipal />
+                </header>
+                <main className={styles.ContainerMain}>
+                    <div className={styles.blocoimgsair}>
+                        <label className={styles.Picture}>
+                            <input type="file" accept='imagem/*' className={styles.PictureInput} />
+                            <div className={styles.ContainerIconiTexto}>
+                                <i><Camera size={40} color="#ff4900" /></i>
+                            </div>
+                        </label>
+                        <div className={styles.ContainerDivButtonSair}>
+                            <button>Sair <ArrowFatLinesRight size={20} /></button>
                         </div>
                     </div>
 
-                    <div className={styles.ContainerInput}>
-                        <div className={styles.ContainerTextoInput}>
-                            <p>Nome do produto</p>
-                            <input type="text" placeholder='adicione aqui'/>
-                        </div>
-                        <div className={styles.ContainerLinha}>
-                            <hr />
-                        </div>
+                    <div className={styles.ContainerDivButton}>
+                        <button>Chat</button>
                     </div>
-
-                    <div className={styles.ContainerInput}>
-                        <div className={styles.ContainerTextoInput}>
-                            <p>Descrição do produto seu produto</p>
-                            <input type="text" placeholder='Descrição do produto...'/>
+                    <form action="">
+                        <div className={styles.ContainerInput}>
+                            <div className={styles.ContainerTextoInput}>
+                                <p>Nome do produto</p>
+                                <input type="text" placeholder='adicione aqui' />
+                            </div>
+                            <div className={styles.ContainerLinha}>
+                                <hr />
+                            </div>
                         </div>
-                        <div className={styles.ContainerLinha}>
-                            <hr />
+
+                        <div className={styles.ContainerInput}>
+                            <div className={styles.ContainerTextoInput}>
+                                <p>Nome do produto</p>
+                                <input type="text" placeholder='adicione aqui' />
+                            </div>
+                            <div className={styles.ContainerLinha}>
+                                <hr />
+                            </div>
                         </div>
-                    </div>
 
-                    <div className={styles.ContainerInput}>
-                        <div className={styles.ContainerTextoInput}>
-                            <p>Nome do produto</p>
-                            <input type="text" placeholder='adicione aqui'/>
+                        <div className={styles.ContainerInput}>
+                            <div className={styles.ContainerTextoInput}>
+                                <p>Descrição do produto seu produto</p>
+                                <input type="text" placeholder='Descrição do produto...' />
+                            </div>
+                            <div className={styles.ContainerLinha}>
+                                <hr />
+                            </div>
                         </div>
-                        <div className={styles.ContainerLinha}>
-                            <hr />
+
+                        <div className={styles.ContainerInput}>
+                            <div className={styles.ContainerTextoInput}>
+                                <p>Nome do produto</p>
+                                <input type="text" placeholder='adicione aqui' />
+                            </div>
+                            <div className={styles.ContainerLinha}>
+                                <hr />
+                            </div>
                         </div>
+                    </form>
+
+                    <div className={styles.ContainerButtons}>
+                        <div className={styles.ContainerAdicionarNovoProduto}>
+                            <button>Adicionar novo produto</button>
+                        </div>
+
+                        <div className={styles.ContainerSalvar}>
+                            <button>Salvar</button>
+                        </div>
+
                     </div>
-                </form>
+                </main>
 
-                <div className={styles.ContainerButtons}>
-                    <div className={styles.ContainerAdicionarNovoProduto}>
-                        <button>Adicionar novo produto</button>
-                    </div>
-
-                    <div className={styles.ContainerSalvar}>
-                        <button>Salvar</button>
-                    </div>
-
-                </div>
-            </main>
-
-            <footer className={styles.ContainerFooter}>
-                <FooterPrincipal/>
-            </footer>
-        </body>
+                <footer className={styles.ContainerFooter}>
+                    <FooterPrincipal />
+                </footer>
+            </body>
         </>
     )
 }
