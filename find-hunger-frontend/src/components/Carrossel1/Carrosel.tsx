ba036870c5be47dfdd80e89508a6ac90b3179fa1
Milton Cardim <miltosocardim19@gmail.com>
2025-02-10 11:09:29 -0300

    Carrosel alterado

--- find-hunger-frontend/src/components/Carrossel1/Carrosel.tsx
+++ find-hunger-frontend/src/components/Carrossel1/Carrosel.tsx
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import Slider from "react-slick";
-import ImgEletrcista from "../../assets/Eletricista.jpeg"
-import ImgPintor from "../../assets/Pintor.jpeg"
-import ImgTecnico from "../../assets/Tecnico.jpeg"
-import ImgMaquiagem from "../../assets/Maquiagem.jpeg"
+import ImgHamburguer from "../../assets/ImgHamburguer2.png"
+import ImgPizza from "../../assets/imgPizza1.png"
+import ImgBatatafrita from "../../assets/ImgBatataFrita3.png"
+import ImgRefrigerantes from "../../assets/ImgRefrigerante4.png"
 
 import styles from './Carrosel.module.css'
 
@@ -18,7 +18,7 @@ export function Carrosel() {
   };
   return (
     <div className={styles.slidercontainer}>
-      <h3 className={styles.titulo}> Comerciantes Populares </h3>
+      <h3 className={styles.titulo}> Tópicos populares </h3>
       <div className={styles.divSlides}
         style={{
           display: display ? "block" : "flex",
@@ -28,33 +28,33 @@ export function Carrosel() {
           <div>
             <div className={styles.divSecIcon}>
               <div className={styles.divIcon}>
-                <img className={styles.img} src={ImgEletrcista} alt="" />
+                <img className={styles.img} src={ImgHamburguer} alt="" />
               </div>
-              <span className={styles.paragrafoCarrosel}>Eletricista</span>
+              <span className={styles.paragrafoCarrosel}>Hamburguer</span>
             </div>
           </div>
           <div>
             <div className={styles.divSecIcon}>
               <div className={styles.divIcon}>
-                <img className={styles.img} src={ImgPintor} alt="" />
+                <img className={styles.img} src={ImgPizza} alt="" />
               </div>
-              <span className={`${styles.paragrafoCarrosel} ${styles.paragrafoEspecial}`}>Pintor</span>
+              <span className={`${styles.paragrafoCarrosel} ${styles.paragrafoEspecial}`}>Pizza</span>
             </div>
           </div>
           <div>
             <div className={styles.divSecIcon}>
               <div className={styles.divIcon}>
-                <img className={styles.img} src={ImgTecnico} alt="" />
+                <img className={styles.img} src={ImgBatatafrita} alt="" />
               </div>
-              <span className={`${styles.paragrafoCarrosel} ${styles.paragrafoEspecial}`}>Técnico</span>
+              <span className={`${styles.paragrafoCarrosel} ${styles.paragrafoEspecial}`}>Batatas</span>
             </div>
           </div>
           <div>
             <div className={styles.divSecIcon}>
               <div className={styles.divIcon}>
-                <img className={styles.img} src={ImgMaquiagem} alt="" />
+                <img className={styles.img} src={ImgRefrigerantes} alt="" />
               </div>
-              <span className={styles.paragrafoCarrosel}>Maquiagem</span>
+              <span className={styles.paragrafoCarrosel}>Bebidas</span>
             </div>
           </div>
         </Slider>
